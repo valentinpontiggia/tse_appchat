82cea32edb984e67cdb488ddbee18cd5691535e0
Valentin <valentin.pontiggia@telecom-st-etienne.fr>
2023-01-18 00:13:23 +0100

    adding the template of the app and room managing

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/*const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
@@ -60,4 +60,53 @@ io.on('connection', (socket) => {
 
 server.listen(3000, () => {
     console.log('listening on :3000');
+});*/
+
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require("socket.io");
+const formatMessage = require('./utils/messages')
+const {userJoin, getCurrentUser, userLeave, getRoomUsers} = require('./utils/users')
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const botName = 'ChatCord Bot';
+
+io.on('connection', (socket) => {
+    socket.on('joinRoom',({ username, room}) => {
+        const user = userJoin(socket.id, username, room);
+        socket.join(user.room);
+
+        socket.emit('message', formatMessage(botName,'Welcome here'));
+        socket.broadcast.to(user.room).emit('message', formatMessage(botName, user.username+ ' has joined the chat'));
+
+        io.to(user.room).emit('roomUsers',{room : user.room, users: getRoomUsers(user.room)});
+    });
+    socket.on('disconnect', ()=> {
+        const user = userLeave(socket.id);
+
+        if(user){
+        io.to(user.room).emit('message', formatMessage(botName, + user.username+ ' has left the chat'));
+        io.to(user.room).emit('roomUsers',{room : user.room, users: getRoomUsers(user.room)});
+        }
+    });
+
+    socket.on('chatMessage', (msg) => {
+        const user = getCurrentUser(socket.id);
+        io.to(user.room).emit('message', formatMessage(user.username,msg));
+    });
+
+    /*socket.on('typing', (username)=>{
+        const user = getCurrentUser(socket.id);
+        io.to(user.room).broadcast.emit('typing', username) ;         
+    });*/
 });
+
+const PORT = 3000;
+
+server.listen(PORT, ()=>console.log(`Server running on ${PORT}`));
