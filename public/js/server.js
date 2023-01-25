dcd934fdfaa1271269933b71e1e59feeb692162f
dhoop.matthieu <matthieu.dhoop@gmail.com>
2023-01-25 16:30:49 +0100

    moved server.js in js folder

--- public/js/server.js
+++ public/js/server.js
@@ -66,8 +66,8 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require("socket.io");
-const {formatMessage, formatPrivateMessage} = require('./utils/messages')
-const {userJoin, getCurrentUser, userLeave, getRoomUsers} = require('./utils/users')
+const {formatMessage, formatPrivateMessage} = require('../../utils/messages')
+const {userJoin, getCurrentUser, userLeave, getRoomUsers} = require('../../utils/users')
 
 const app = express();
 const server = http.createServer(app);
